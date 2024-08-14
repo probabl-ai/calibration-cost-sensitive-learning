572c88bab2613f4b335810ad962c91a62db73273
Guillaume Lemaitre <guillaume@probabl.ai>
2024-08-14 15:49:34 +0200

    EXO build your own calibration curve (#3)

--- python_files/_generate_predictions.py
+++ python_files/_generate_predictions.py
@@ -4,6 +4,7 @@ from pathlib import Path
 Path.mkdir(Path.cwd() / "predictions", exist_ok=True)
 
 # %%
+import numpy as np
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 
@@ -11,6 +12,7 @@ X, y = make_classification(
     n_samples=100_000, class_sep=0.5, random_state=0
 )
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+np.save("./predictions/y_true.npy", y_test)
 
 # %%
 from sklearn.linear_model import LogisticRegression
@@ -22,8 +24,6 @@ model.fit(X_train, y_train)
 y_pred = model.predict_proba(X_test)[:, 1]
 
 # %%
-import numpy as np
-
 np.save("./predictions/y_prob_1.npy", y_pred)
 
 # %%
