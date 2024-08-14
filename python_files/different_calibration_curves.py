7c09dd4f97523121872609c6d91db8189055f990
Guillaume Lemaitre <guillaume@probabl.ai>
2024-08-14 16:28:32 +0200

    iter

--- python_files/different_calibration_curves.py
+++ python_files/different_calibration_curves.py
@@ -1,3 +1,15 @@
+# %% [markdown]
+#
+# # Understanding calibration curves
+#
+# In this notebook, we explore calibration curves. We use different set of predictions
+# leading to different calibration curves from which we want to build insights and
+# understand the impact and meaning when it comes to our predictive models.
+#
+# So let's first generate some predictions. The generative process is located in the
+# file `_generate_predictions.py`. This process stores the true labels and the
+# predicted probability estimates of several models into the `predictions` folder.
+
 # %%
 # Equivalent to the magic command "%run _generate_predictions.py" but it allows this
 # file to be executed as a Python script.
@@ -6,9 +18,40 @@ from IPython import get_ipython
 ipython = get_ipython()
 ipython.run_line_magic("run", "_generate_predictions.py")
 
+# %% [markdown]
+#
+# We first load the true testing labels of our problem.
+
 # %%
 import numpy as np
 
 y_true = np.load("./predictions/y_true.npy")
+y_true
+
+# %% [markdown]
+#
+# We observed that we have a binary classification problem. Now, we load different
+# sets of predictions of probabilities estimated by different models.
+
+# %%
+y_proba = np.load("./predictions/y_prob_1.npy")
+y_proba
+
+# %% [markdown]
+#
+# We assess the calibration of the model that provide these predictions by plotting
+# the calibration curve.
+
+# %%
+from sklearn.calibration import CalibrationDisplay
+
+params = {"n_bins": 10, "strategy": "quantile"}
+disp = CalibrationDisplay.from_predictions(y_true, y_proba, **params)
+_ = disp.ax_.set(
+    title="Calibrated model",
+    xlim=(0, 1),
+    ylim=(0, 1),
+    aspect="equal",
+)
 
 # %%
