2c38882e0dd159381cae7a2e69bf8ab434fcaa25
Guillaume Lemaitre <guillaume@probabl.ai>
2024-08-14 14:24:53 +0200

    we generate 5 files now

--- tests/check_generated_predictions.py
+++ tests/check_generated_predictions.py
@@ -5,4 +5,4 @@ path_to_predictions = Path(Path.cwd() / ".." / "python_files" / "predictions")
 assert path_to_predictions.exists()
 
 # %%
-assert len(list(path_to_predictions.glob("*.npy"))) == 4
+assert len(list(path_to_predictions.glob("*.npy"))) == 5
