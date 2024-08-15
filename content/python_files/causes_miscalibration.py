9e816d3eb8898fb5a493ba76d571185b9676a3e1
Guillaume Lemaitre <guillaume@probabl.ai>
2024-08-15 12:36:31 +0200

    show effect of resampling

--- content/python_files/causes_miscalibration.py
+++ content/python_files/causes_miscalibration.py
@@ -59,3 +59,72 @@ from sklearn.calibration import CalibrationDisplay
 CalibrationDisplay.from_estimator(model, X, y, strategy="quantile", n_bins=10)
 
 # %%
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+X, y = make_classification(
+    n_samples=20_000,
+    n_features=2,
+    n_redundant=0,
+    weights=[0.1, 0.9],
+    class_sep=1,
+    random_state=1,
+)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
+
+# %%
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# %%
+from sklearn.metrics import classification_report
+
+print(classification_report(y_test, model.predict(X_test)))
+
+# %%
+_, ax = plt.subplots()
+DecisionBoundaryDisplay.from_estimator(
+    model,
+    X_test,
+    ax=ax,
+    cmap="coolwarm",
+    response_method="predict",
+    alpha=0.5,
+)
+ax.scatter(*X_test.T, c=y_test, cmap="coolwarm", alpha=0.5)
+ax.set(xlabel="Feature 1", ylabel="Feature 2")
+
+# %%
+model.set_params(class_weight="balanced").fit(X_train, y_train)
+
+# %%
+from sklearn.metrics import classification_report
+
+print(classification_report(y_test, model.predict(X_test)))
+
+# %%
+_, ax = plt.subplots()
+DecisionBoundaryDisplay.from_estimator(
+    model,
+    X_test,
+    ax=ax,
+    cmap="coolwarm",
+    response_method="predict",
+    alpha=0.5,
+)
+ax.scatter(*X_test.T, c=y_test, cmap="coolwarm", alpha=0.5)
+ax.set(xlabel="Feature 1", ylabel="Feature 2")
+
+# %%
+model_vanilla = LogisticRegression().fit(X_train, y_train)
+model_reweighted = LogisticRegression(class_weight="balanced").fit(X_train, y_train)
+
+# %%
+disp = CalibrationDisplay.from_estimator(
+    model_vanilla, X_test, y_test, strategy="quantile"
+)
+CalibrationDisplay.from_estimator(
+    model_reweighted, X_test, y_test, strategy="quantile", ax=disp.ax_
+)
+
+# %%
