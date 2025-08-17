4ed4204ac08a60efe407beb497b442d0fb1749f4
Olivier Grisel <olivier.grisel@ensta.org>
2025-08-17 17:26:10 +0200

    Fix matplotlib import for jupyterlite

--- content/python_files/imbalanced_classification.py
+++ content/python_files/imbalanced_classification.py
@@ -48,6 +48,7 @@
 #   probability of the positive class we previously computed.
 
 # %%
+import matplotlib.pyplot as plt  # needed for pandas in jupyterlite.
 import numpy as np
 import pandas as pd
 from scipy.special import expit
@@ -368,8 +369,8 @@ print(classification_report(y, model.predict(X)))
 # regression model.
 
 # %%
-from imblearn.under_sampling import RandomUnderSampler
 from imblearn.pipeline import make_pipeline
+from imblearn.under_sampling import RandomUnderSampler
 
 # keep a 0.7 ratio between the number of samples of the rare event and the number of
 # samples of the majority event.
@@ -678,7 +679,6 @@ fig_plotly.show()
 
 # %%
 from sklearn.model_selection import FixedThresholdClassifier
-
 
 # %% [markdown]
 #
